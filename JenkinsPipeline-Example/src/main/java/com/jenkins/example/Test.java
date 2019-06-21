b44c6f001780dfe25a8e53a67ad2301eedeb2240
Manvitha <manvithareddy69@gmail.com>
2019-06-21 16:51:23 +0530

    First Commit

---
+++ JenkinsPipeline-Example/src/main/java/com/jenkins/example/Test.java
@@ -0,0 +1,11 @@
+package com.jenkins.example;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           
+		System.out.println("Welcome to Hello World.........");
+	}
+
+}
